d5d9978bf2eede29537fe0326e1cb9fd0b673b35
HugoRCD <hugo.richard@epitech.eu>
2025-07-02 09:49:33 +0200

    up

--- app/app.vue
+++ app/app.vue
@@ -3,7 +3,11 @@ useSeoMeta({
   title: 'Nuxt x BetterAuth',
   description: 'Nuxt x BetterAuth - Demo of using BetterAuth with Nuxt',
   ogImage: 'https://better-auth.hrcd.fr/og.png',
+  ogUrl: 'https://better-auth.hrcd.fr',
+  twitterCard: 'summary_large_image',
   twitterImage: 'https://better-auth.hrcd.fr/og.png',
+  twitterTitle: 'Nuxt x BetterAuth',
+  twitterDescription: 'Nuxt x BetterAuth - Demo of using BetterAuth with Nuxt',
 })
 </script>
 
